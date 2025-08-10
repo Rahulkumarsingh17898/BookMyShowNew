d3ca753cf051c014b3d65f7a9a898b80719a49c3
rahulkumarsingh <rahulkumarsingh17898@gmail.com>
2025-08-10 23:40:20 +0530

    Structure Of Code Added

---
+++ src/test/java/com/rkstech/bookyourshow/BookyourshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkstech.bookyourshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookyourshowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
